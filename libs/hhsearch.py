8a807dff2bd62ae8ee47b7f0f1649935b0291c29
Josep Triviño <peptrivi@gmail.com>
2022-10-25 12:25:50 -0300

    changed database

--- libs/hhsearch.py
+++ libs/hhsearch.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import shutil
 import subprocess
 from libs import utils
 
@@ -10,8 +11,8 @@ def create_database_from_pdb(fasta_path: str, database_path: str, output_dir: st
     utils.create_dir(database_dir, delete_if_exists=True)
     subprocess.Popen(['ffindex_from_fasta','-s', f'{database_dir}/template.ffdata', f'{database_dir}/template.ffindex', fasta_path])
     subprocess.Popen(['hhblits_omp','-i', f'{database_dir}/template', '-d', database_path, '-oa3m', f'{database_dir}/template_a3m_wo_ss','-n','2','-v','0'])
-    os.rename(os.path.join(database_dir, 'template_a3m_wo_ss.ffdata'), os.path.join(database_dir, 'template_a3m.ffdata'))
-    os.rename(os.path.join(database_dir, 'template_a3m_wo_ss.ffindex'), os.path.join(database_dir, 'template_a3m.ffindex'))
+    shutil.copy2(os.path.join(database_dir, 'template_a3m_wo_ss.ffdata'), os.path.join(database_dir, 'template_a3m.ffdata'))
+    shutil.copy2(os.path.join(database_dir, 'template_a3m_wo_ss.ffindex'), os.path.join(database_dir, 'template_a3m.ffindex'))
     subprocess.Popen(['ffindex_apply',f'{database_dir}/template_a3m.ff{{data,index}}','-i', f'{database_dir}/template_hhm.ffindex','-d', f'{database_dir}/template_hhm.ffdata','--','hhmake','-i','stdin','-o','stdout','-v','0'])
     subprocess.Popen(['cstranslate','-f','-x','0.3','-c','4','-I','a3m','-i',f'{database_dir}/template_a3m','-o',f'{database_dir}/template_cs219'])
     return database_dir
