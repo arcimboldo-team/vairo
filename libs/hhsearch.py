d99064eff0f79f3a8d8c34f3d50b091b308ef492
Josep Triviño <peptrivi@gmail.com>
2022-10-25 12:46:15 -0300

    changed database

--- libs/hhsearch.py
+++ libs/hhsearch.py
@@ -9,18 +9,19 @@ def create_database_from_pdb(fasta_path: str, database_path: str, output_dir: st
     name = utils.get_file_name(fasta_path)
     database_dir = os.path.join(output_dir, f'{name}_database')
     utils.create_dir(database_dir, delete_if_exists=True)
-    subprocess.call(['ffindex_from_fasta','-s', f'{database_dir}/template.ffdata', f'{database_dir}/template.ffindex', fasta_path])
-    subprocess.call(['hhblits_omp','-i', f'{database_dir}/template', '-d', database_path, '-oa3m', f'{database_dir}/template_a3m_wo_ss','-n','2','-v','0'])
-    shutil.copy2(os.path.join(database_dir, 'template_a3m_wo_ss.ffdata'), os.path.join(database_dir, 'template_a3m.ffdata'))
-    shutil.copy2(os.path.join(database_dir, 'template_a3m_wo_ss.ffindex'), os.path.join(database_dir, 'template_a3m.ffindex'))
-    subprocess.call(['ffindex_apply',f'{database_dir}/template_a3m.ffdata',f'{database_dir}/template_a3m.ffindex','-i', f'{database_dir}/template_hhm.ffindex','-d', f'{database_dir}/template_hhm.ffdata','--','hhmake','-i','stdin','-o','stdout','-v','0'])
-    subprocess.call(['cstranslate','-f','-x','0.3','-c','4','-I','a3m','-i',f'{database_dir}/template_a3m','-o',f'{database_dir}/template_cs219'])
+    subprocess.call(['ffindex_from_fasta','-s', f'{database_dir}/{name}.ffdata', f'{database_dir}/{name}.ffindex', fasta_path])
+    subprocess.call(['hhblits_omp','-i', f'{database_dir}/{name}', '-d', database_path, '-oa3m', f'{database_dir}/{name}_a3m_wo_ss','-n','2','-v','0'])
+    shutil.copy2(os.path.join(database_dir, f'{name}_a3m_wo_ss.ffdata'), os.path.join(database_dir, f'{name}_a3m.ffdata'))
+    shutil.copy2(os.path.join(database_dir, f'{name}_a3m_wo_ss.ffindex'), os.path.join(database_dir, f'{name}_a3m.ffindex'))
+    subprocess.call(['ffindex_apply',f'{database_dir}'/f'{name}_a3m.ffdata',f'{database_dir}/{name}_a3m.ffindex','-i', f'{database_dir}/{name}_hhm.ffindex','-d', f'{database_dir}/{name}_hhm.ffdata','--','hhmake','-i','stdin','-o','stdout','-v','0'])
+    subprocess.call(['cstranslate','-f','-x','0.3','-c','4','-I','a3m','-i',f'{database_dir}/{name}_a3m','-o',f'{database_dir}/{name}_cs219'])
     return database_dir
 
 def run_hhsearch(fasta_path: str, database_path, output_path: str) -> str:
 
+    name = utils.get_file_name(fasta_path)
     out = subprocess.Popen(['hhsearch','-i',fasta_path,'-o',output_path,'-maxseq',
-                            '1000000','-d',database_path,'-p','20','-Z','250','-loc','-z','1',
+                            '1000000','-d',os.path.join(database_path,name),'-p','20','-Z','250','-loc','-z','1',
                             '-b','1','-B','250','-ssm','2','-sc','1','-seq','1','-dbstrlen','10000',
                             '-norealign','-maxres','32000'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     stdout, stderr = out.communicate()
