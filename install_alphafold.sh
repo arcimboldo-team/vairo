ff8c2eb8c084055a1a8743d859dfaa5cbabcdfe0
Josep Triviño <peptrivi@gmail.com>
2022-11-14 12:49:55 -0300

    updated file

--- install_alphafold.sh
+++ install_alphafold.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/bash -i
 
-conda create -n af python=3.7
+eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
+conda create -y -n af python=3.7
 conda activate af
 conda install -y -c conda-forge openmm==7.5.1 cudnn==8.2.1.32 cudatoolkit==11.0.3 pdbfixer==1.7
 conda install -y -c bioconda hmmer==3.3.2 hhsuite==3.3.0 kalign2==2.04
@@ -9,5 +10,7 @@ pip install --upgrade jax==0.2.14 jaxlib==0.1.69+cuda111 -f https://storage.goog
 pip install git+https://github.com/deepmind/alphafold.git@v2.1.2
 path=$(python -c 'import site; print(site.getsitepackages()[0])')
 cd $path
-curl https://raw.githubusercontent.com/deepmind/alphafold/main/docker/openmm.patch -o /tmp/openmm.patch
+tmpfile=$(mktemp /tmp/openmm.XXXXXX)
+curl https://raw.githubusercontent.com/deepmind/alphafold/main/docker/openmm.patch -o $tmpfile
 patch -p0 < /tmp/openmm.patch
+rm $tmpfile
