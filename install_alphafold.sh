5b0c9dc493273dbd9de88baa27c02b277aa42f17
Josep Triviño <peptrivi@gmail.com>
2022-11-17 16:16:22 -0300

    updated tensorflow

--- install_alphafold.sh
+++ install_alphafold.sh
@@ -5,7 +5,7 @@ conda create -y -n af python=3.8
 conda activate af
 conda install -y -c conda-forge openmm==7.5.1 cudatoolkit==11.2 cudnn==8.2.1.32 pdbfixer==1.7
 conda install -y -c bioconda hmmer==3.3.2 hhsuite==3.3.0 kalign2==2.04
-pip install absl-py==1.0.0 biopython==1.79 chex==0.0.7 dm-haiku==0.0.7 dm-tree==0.1.6 immutabledict==2.0.0 ml-collections==0.1.0 numpy==1.21.6 scipy==1.7.0 protobuf==3.20.1 pandas==1.3.4 tensorflow-cpu==2.9.0 pyyaml future matplotlib python-igraph csb psutil paramiko scikit-learn pickle5
+pip install absl-py==1.0.0 biopython==1.79 chex==0.0.7 dm-haiku==0.0.7 dm-tree==0.1.6 immutabledict==2.0.0 ml-collections==0.1.0 numpy==1.21.6 scipy==1.7.0 protobuf==3.20.1 pandas==1.3.4 tensorflow==2.5.0 tensorflow-cpu==2.9.0 pyyaml future matplotlib python-igraph csb psutil paramiko scikit-learn pickle5
 pip install jax==0.3.17 jaxlib==0.3.15+cuda11.cudnn82 -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 pip install git+https://github.com/deepmind/alphafold.git@v2.2.4
 path=$(python -c 'import site; print(site.getsitepackages()[0])')
