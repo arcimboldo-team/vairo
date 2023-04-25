e06ef321edb94543ce0ff38a7d29e9325dee50c1
Josep Triviño <jtvcri@ibmb.csic.es>
2023-04-25 16:35:37 +0200

    Align (#49)
    * fixing alignment
    
    * fix alignment
    
    * fix alignment

--- libs/hhsearch.py
+++ libs/hhsearch.py
@@ -8,20 +8,21 @@ from libs import utils
 def create_database_from_pdb(fasta_path: str, database_path: str, output_dir: str) -> str:
     name = utils.get_file_name(fasta_path)
     database_dir = os.path.join(output_dir, f'{name}_database')
+    data_name = os.path.join(database_dir, name)
     utils.create_dir(database_dir, delete_if_exists=True)
     subprocess.call(
-        ['ffindex_from_fasta', '-s', f'{database_dir}/{name}.ffdata', f'{database_dir}/{name}.ffindex', fasta_path])
-    subprocess.call(['hhblits_omp', '-i', f'{database_dir}/{name}', '-d', database_path, '-oa3m',
-                     f'{database_dir}/{name}_a3m_wo_ss', '-n', '2', '-cpu', '4', '-v', '0'])
+        ['ffindex_from_fasta', '-s', f'{data_name}.ffdata', f'{data_name}.ffindex', fasta_path])
+    subprocess.call(['hhblits_omp', '-i', f'{data_name}', '-d', database_path, '-oa3m',
+                     f'{data_name}_a3m_wo_ss', '-n', '2', '-cpu', '4', '-v', '0'])
     shutil.copy2(os.path.join(database_dir, f'{name}_a3m_wo_ss.ffdata'),
                  os.path.join(database_dir, f'{name}_a3m.ffdata'))
     shutil.copy2(os.path.join(database_dir, f'{name}_a3m_wo_ss.ffindex'),
                  os.path.join(database_dir, f'{name}_a3m.ffindex'))
-    subprocess.call(['ffindex_apply', f'{database_dir}/{name}_a3m.ffdata', f'{database_dir}/{name}_a3m.ffindex', '-i',
-                     f'{database_dir}/{name}_hhm.ffindex', '-d', f'{database_dir}/{name}_hhm.ffdata', '--', 'hhmake',
+    subprocess.call(['ffindex_apply', f'{data_name}_a3m.ffdata', f'{data_name}_a3m.ffindex', '-i',
+                     f'{data_name}_hhm.ffindex', '-d', f'{data_name}_hhm.ffdata', '--', 'hhmake',
                      '-i', 'stdin', '-o', 'stdout', '-v', '0'])
-    subprocess.call(['cstranslate', '-f', '-x', '0.3', '-c', '4', '-I', 'a3m', '-i', f'{database_dir}/{name}_a3m', '-o',
-                     f'{database_dir}/{name}_cs219'])
+    subprocess.call(['cstranslate', '-f', '-x', '0.3', '-c', '4', '-I', 'a3m', '-i', f'{data_name}_a3m', '-o',
+                     f'{data_name}_cs219'])
     return os.path.join(database_dir, name)
 
 
