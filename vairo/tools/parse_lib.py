076efe0be92f290e43371d88d599aad946da0a3d
Pep Triviño <peptrivi@gmail.com>
2023-10-06 15:43:49 +0200

    fixed

--- vairo/tools/parse_lib.py
+++ vairo/tools/parse_lib.py
@@ -7,9 +7,11 @@ from Bio.PDB.Residue import Residue
 
 
 def get_coordinates(struct):
-    for chain in struct[0]:
-        for residue in chain:
-            return residue['CA']
+    num_residues = len(list(struct.get_residues()))
+    middle_index = num_residues // 2
+    residue = list(struct.get_residues())[middle_index]
+    return residue['CA']
+
 
 def read_file(path):
     parser = PDBParser()
@@ -22,25 +24,25 @@ def read_file(path):
             atom_started = True
             filtered_lines.append(line)
         elif atom_started and line.startswith("TER"):
-            splitted_file.append(parser.get_structure("pdb", StringIO(''.join(filtered_lines))))           
+            splitted_file.append(parser.get_structure("pdb", StringIO(''.join(filtered_lines))))
             filtered_lines = []
             atom_started = False
 
     return splitted_file
 
+
 # Your PDB data
-pdb_path_reference = "/Users/pep/work/transfers/BETA_lib_uud/1/0/1vdv_0_1.pdb"
+pdb_path_reference = "/cri4/pep/Downloads/lib/BETA_lib_uud/3/0/1gwy_0_6.pdb"
 pdb_data_reference = read_file(pdb_path_reference)
 
-pdb_path_obj = "/Users/pep/work/transfers/BETA_lib_uud/1/0/3tmr_0_66.pdb"
+pdb_path_obj = "/cri4/pep/Downloads/lib/BETA_lib_uud/1/1/1d7f_0_9.pdb"
 pdb_data_obj = read_file(pdb_path_obj)
-
 
 # Initialize a PDB parser
 
-correct_order = []
+correct_order = [0] * len(pdb_data_obj)
 for segment in pdb_data_obj:
-    min_distance = float('inf')  # Initialize with a large value
+    min_distance = float('inf')
     closest_coordinate = None
     coord_obj = get_coordinates(segment)
     for num, reference_struct in enumerate(pdb_data_reference):
@@ -48,19 +50,17 @@ for segment in pdb_data_obj:
         if distance < min_distance:
             min_distance = distance
             closest_coordinate = num
-    correct_order.append(closest_coordinate)
+    correct_order[closest_coordinate] = segment
 
 merged_structure = Structure("merged")
 merged_model = Model(0)
 merged_chain = Chain('A')
 
-
 residue_id = 1
-for order in correct_order:
-    add_struct = pdb_data_obj[order]
+for add_struct in correct_order:
     for residue in Selection.unfold_entities(add_struct, 'R'):
         new_residue = Residue((' ', residue_id, ' '), residue.get_resname(), '')
-        
+
         # Copy atoms from the original residue to the new residue
         for atom in residue:
             new_atom = atom.copy()
@@ -79,4 +79,3 @@ merged_structure.add(merged_model)
 io = PDBIO()
 io.set_structure(merged_structure)
 io.save("merged_structure.pdb")
-    
