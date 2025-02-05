b66235e72c3c8da5f9e54bd586ad125f1b9aad54
jtvcri <peptrivi@gmail.com>
2025-02-05 16:52:09 +0100

    small fix

--- vairo/tools/install_vairo.sh
+++ vairo/tools/install_vairo.sh
@@ -55,17 +55,18 @@ fi
 
 echo 'Creating VAIRO Conda environment.'
 read -p "Enter the Conda environment name: " env_name
-conda create -y -n "$env_name" python=3.8
+conda create -y -n "$env_name" python=3.11
 conda activate "$env_name"
-conda install -y -c conda-forge openmm==7.5.1 cudatoolkit==11.2 cudnn==8.2.1.32 pdbfixer==1.7
-conda install -y -c bioconda hmmer==3.3.2 hhsuite==3.3.0 kalign2==2.04
+conda install -y -c conda-forge openmm==8.0.0 cudatoolkit==11.2 pdbfixer
+conda install -y -c bioconda hmmer hhsuite==3.3.0 kalign2
+conda clean --all --force-pkgs-dirs --yes
 
 # Check if ptxas command exists
 echo "Checking ptxas..."
 if command -v ptxas &> /dev/null; then
     echo "ptxas command is installed. Skipping cudatoolkit-dev installation."
 else
-  if conda install -y --quiet -c conda-forge "cudatoolkit-dev=11.2"; then
+  if conda install -y --quiet -c conda-forge "cudatoolkit-dev=11.7"; then
       echo "Cudatoolkit-dev installed."
   else
       echo "Conda installation of cudatoolkit-dev failed. In order to continue, download and install cudatoolkit: https://developer.nvidia.com/cuda-toolkit"
@@ -73,16 +74,11 @@ else
   fi
 fi
 
-pip install absl-py==1.0.0 biopython==1.79 chex==0.0.7 dm-haiku==0.0.9 dm-tree==0.1.6 immutabledict==2.0.0 ml-collections==0.1.0 numpy==1.21.6 scipy==1.7.0 protobuf==3.20.1 pandas==1.3.4 tensorflow==2.9.0 tensorflow-cpu==2.9.0 matplotlib==3.6.2 python-igraph==0.9.10 pyyaml future csb psutil paramiko scikit-learn pickle5 jinja2 flask
-pip install jax==0.3.25 jaxlib==0.3.25+cuda11.cudnn805 -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
-pip install git+https://github.com/deepmind/alphafold.git@v2.2.4
+pip install absl-py==1.0.0 biopython==1.79 chex==0.1.86 dm-haiku==0.0.12 dm-tree==0.1.8 immutabledict==2.0.0 ml-collections==0.1.0 numpy==1.24.3 scipy==1.11.1 pandas==1.3.4 tensorflow==2.16.1 tensorflow-cpu==2.16.1 matplotlib==3.6.2 python-igraph pyyaml future csb psutil paramiko scikit-learn jinja2 flask
+pip install git+https://github.com/deepmind/alphafold.git
 path=$(python -c "import site; print(site.getsitepackages()[0])")
 cd "$path" || exit
-tmpfile=$(mktemp /tmp/openmm.XXXXXX)
-curl https://raw.githubusercontent.com/deepmind/alphafold/v2.2.4/docker/openmm.patch -o "$tmpfile"
-patch -p0 < "$tmpfile"
-rm "$tmpfile"
 wget -q -P "${path}"/alphafold/common/ https://git.scicore.unibas.ch/schwede/openstructure/-/raw/7102c63615b64735c4941278d92b554ec94415f8/modules/mol/alg/src/stereo_chemical_props.txt
-
+pip install jax==0.4.26 jaxlib==0.4.26+cuda12.cudnn89 -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 echo "A conda environment with the name ""$env_name"" has been created. To run VAIRO, you must first activate the new environment (conda activate ""$env_name"") and type VAIRO. This will run the program and show the different options for creating a job."
 echo "Before running VAIRO, please ensure that the libraries of AlphaFold2 are installed. If they have not been installed, please download them using the following script.: https://github.com/deepmind/alphafold/blob/v2.2.4/scripts/download_all_data.sh"
