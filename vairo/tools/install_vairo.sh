cd47f9c00724f1ebcfa56162b32c36114bd98143
jtvcri <peptrivi@gmail.com>
2025-02-12 09:13:46 +0100

    updated af2

--- vairo/tools/install_vairo.sh
+++ vairo/tools/install_vairo.sh
@@ -57,8 +57,14 @@ echo 'Creating VAIRO Conda environment.'
 read -p "Enter the Conda environment name: " env_name
 conda create -y -n "$env_name" python=3.11
 conda activate "$env_name"
-conda install -y -c conda-forge openmm==8.0.0 cudatoolkit==11.2 pdbfixer
+conda install -y -c conda-forge openmm==8.0.0 pdbfixer
 conda install -y -c bioconda hmmer hhsuite==3.3.0 kalign2
+conda install -c cudnn=9.1
+pip install absl-py==1.0.0 biopython==1.79 chex==0.1.86 dm-haiku==0.0.12 dm-tree==0.1.8 immutabledict==2.0.0 ml-collections==0.1.0 numpy==1.26.4 scipy==1.11.1 pandas==2.0.3 tensorflow==2.16.1 tensorflow-cpu==2.16.1 matplotlib==3.6.2 python-igraph pyyaml future csb psutil paramiko scikit-learn jinja2 flask
+pip3 install --upgrade --no-cache-dir \
+      jax==0.4.29 \
+      jaxlib==0.4.29+cuda12.cudnn91 \
+      -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 conda clean --all --force-pkgs-dirs --yes
 
 # Check if ptxas command exists
@@ -74,11 +80,10 @@ else
   fi
 fi
 
-pip install absl-py==1.0.0 biopython==1.79 chex==0.1.86 dm-haiku==0.0.12 dm-tree==0.1.8 immutabledict==2.0.0 ml-collections==0.1.0 numpy==1.24.3 scipy==1.11.1 pandas==1.3.4 tensorflow==2.16.1 tensorflow-cpu==2.16.1 matplotlib==3.6.2 python-igraph pyyaml future csb psutil paramiko scikit-learn jinja2 flask
-pip install git+https://github.com/deepmind/alphafold.git
+pip install --no-dependencies git+https://github.com/deepmind/alphafold.git
 path=$(python -c "import site; print(site.getsitepackages()[0])")
 cd "$path" || exit
 wget -q -P "${path}"/alphafold/common/ https://git.scicore.unibas.ch/schwede/openstructure/-/raw/7102c63615b64735c4941278d92b554ec94415f8/modules/mol/alg/src/stereo_chemical_props.txt
-pip install jax==0.4.29 jaxlib==0.4.29+cuda12.cudnn91 -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+
 echo "A conda environment with the name ""$env_name"" has been created. To run VAIRO, you must first activate the new environment (conda activate ""$env_name"") and type VAIRO. This will run the program and show the different options for creating a job."
 echo "Before running VAIRO, please ensure that the libraries of AlphaFold2 are installed. If they have not been installed, please download them using the following script.: https://github.com/deepmind/alphafold/blob/v2.2.4/scripts/download_all_data.sh"
